907669eaae185062305f728047d2d03d4af638d6
clavin <clavin@electronjs.org>
2025-01-09 12:51:42 -0700

    add curve constraints

--- shell/renderer/electron_smooth_round_rect.cc
+++ shell/renderer/electron_smooth_round_rect.cc
@@ -11,6 +11,7 @@ namespace electron {
 
 namespace {
 
+// Applies quarter rotations (n * 90°) to a point relative to the origin.
 constexpr SkPoint QuarterRotate(const SkPoint& p,
                                 unsigned int quarter_rotations) {
   switch (quarter_rotations % 4) {
@@ -26,6 +27,20 @@ constexpr SkPoint QuarterRotate(const SkPoint& p,
   }
 }
 
+// Edge length consumed for a given smoothness and corner radius.
+constexpr float LengthForCornerSmoothness(float smoothness, float radius) {
+  return (1.0f + smoothness) * radius;
+}
+
+// The smoothness value when consuming an edge length for a corner with a given
+// radius.
+//
+// This function complements `LengthForCornerSmoothness`:
+//   SmoothnessForCornerLength(LengthForCornerSmoothness(s, r), r) = s
+constexpr float SmoothnessForCornerLength(float length, float radius) {
+  return (length / radius) - 1.0f;
+}
+
 // Geometric measurements for constructing the curves of smooth round corners on
 // a rectangle.
 //
@@ -38,18 +53,18 @@ constexpr SkPoint QuarterRotate(const SkPoint& p,
 // For vectors, the X direction is parallel and the Y direction is
 // perpendicular.
 struct CurveGeometry {
-  CurveGeometry(float radius, float smoothness);
+  constexpr CurveGeometry(float radius, float smoothness);
 
-  inline constexpr SkVector edge_connecting_vector() const {
+  constexpr SkVector edge_connecting_vector() const {
     return {edge_connecting_offset, 0.0f};
   }
-  inline constexpr SkVector edge_curve_vector() const {
+  constexpr SkVector edge_curve_vector() const {
     return {edge_curve_offset, 0.0f};
   }
-  inline constexpr SkVector arc_curve_vector() const {
+  constexpr SkVector arc_curve_vector() const {
     return {arc_curve_offset, 0.0f};
   }
-  inline constexpr SkVector arc_connecting_vector_transposed() const {
+  constexpr SkVector arc_connecting_vector_transposed() const {
     return {arc_connecting_vector.y(), arc_connecting_vector.x()};
   }
 
@@ -66,8 +81,8 @@ struct CurveGeometry {
   SkVector arc_connecting_vector;
 };
 
-CurveGeometry::CurveGeometry(float radius, float smoothness) {
-  edge_connecting_offset = (1.0f + smoothness) * radius;
+constexpr CurveGeometry::CurveGeometry(float radius, float smoothness) {
+  edge_connecting_offset = LengthForCornerSmoothness(smoothness, radius);
 
   float arc_angle = (std::numbers::pi / 4.0f) * smoothness;
 
@@ -85,14 +100,15 @@ CurveGeometry::CurveGeometry(float radius, float smoothness) {
 
 void DrawCorner(SkPath& path,
                 float radius,
-                const CurveGeometry& curve,
+                const CurveGeometry& curve1,
+                const CurveGeometry& curve2,
                 const SkPoint& corner,
                 unsigned int quarter_rotations) {
   // Move/Line to the edge connecting point
   {
     SkPoint edge_connecting_point =
         corner +
-        QuarterRotate(curve.edge_connecting_vector(), quarter_rotations + 1);
+        QuarterRotate(curve1.edge_connecting_vector(), quarter_rotations + 1);
 
     if (quarter_rotations == 0) {
       path.moveTo(edge_connecting_point);
@@ -103,23 +119,21 @@ void DrawCorner(SkPath& path,
 
   // Draw the first smoothing curve
   {
-    SkPoint edge_curve_point = corner + QuarterRotate(curve.edge_curve_vector(),
-                                                      quarter_rotations + 1);
-    SkPoint arc_curve_point =
-        corner + QuarterRotate(curve.arc_curve_vector(), quarter_rotations + 1);
-    SkPoint arc_connecting_point =
+    SkPoint edge_curve_point =
         corner +
-        QuarterRotate(curve.arc_connecting_vector_transposed(),
-                      quarter_rotations);  // This transpose is equivalent to 3
-                                           // rotations, and 3 + 1 quarter
-                                           // rotations = 0 quarter rotations
+        QuarterRotate(curve1.edge_curve_vector(), quarter_rotations + 1);
+    SkPoint arc_curve_point = corner + QuarterRotate(curve1.arc_curve_vector(),
+                                                     quarter_rotations + 1);
+    SkPoint arc_connecting_point =
+        corner + QuarterRotate(curve1.arc_connecting_vector_transposed(),
+                               quarter_rotations);
     path.cubicTo(edge_curve_point, arc_curve_point, arc_connecting_point);
   }
 
   // Draw the arc
   {
     SkPoint arc_connecting_point =
-        corner + QuarterRotate(curve.arc_connecting_vector, quarter_rotations);
+        corner + QuarterRotate(curve2.arc_connecting_vector, quarter_rotations);
     path.arcTo(SkPoint::Make(radius, radius), 0.0f, SkPath::kSmall_ArcSize,
                SkPathDirection::kCW, arc_connecting_point);
   }
@@ -127,14 +141,43 @@ void DrawCorner(SkPath& path,
   // Draw the second smoothing curve
   {
     SkPoint arc_curve_point =
-        corner + QuarterRotate(curve.arc_curve_vector(), quarter_rotations);
+        corner + QuarterRotate(curve2.arc_curve_vector(), quarter_rotations);
     SkPoint edge_curve_point =
-        corner + QuarterRotate(curve.edge_curve_vector(), quarter_rotations);
+        corner + QuarterRotate(curve2.edge_curve_vector(), quarter_rotations);
     SkPoint edge_connecting_point =
         corner +
-        QuarterRotate(curve.edge_connecting_vector(), quarter_rotations);
+        QuarterRotate(curve2.edge_connecting_vector(), quarter_rotations);
     path.cubicTo(arc_curve_point, edge_curve_point, edge_connecting_point);
   }
+}
+
+// Constrains the smoothness of two corners along the same edge.
+//
+// If the smoothness value needs to be constrained, it will try to keep the
+// ratio of the smoothness values the same as the ratio of the radii
+// (`s1/s2 = r1/r2`).
+constexpr std::pair<float, float> ConstrainSmoothness(float size,
+                                                      float smoothness,
+                                                      float radius1,
+                                                      float radius2) {
+  float edge_consumed1 = LengthForCornerSmoothness(smoothness, radius1);
+  float edge_consumed2 = LengthForCornerSmoothness(smoothness, radius2);
+
+  // If both corners fit within the edge size then keep the smoothness
+  if (edge_consumed1 + edge_consumed2 <= size) {
+    return {smoothness, smoothness};
+  }
+
+  float ratio = radius1 / (radius1 + radius2);
+  float length1 = size * ratio;
+  float length2 = size - length1;
+
+  float smoothness1 =
+      std::max(SmoothnessForCornerLength(length1, radius1), 0.0f);
+  float smoothness2 =
+      std::max(SmoothnessForCornerLength(length2, radius2), 0.0f);
+
+  return {smoothness1, smoothness2};
 }
 
 }  // namespace
@@ -148,38 +191,52 @@ SkPath DrawSmoothRoundRect(float x,
                            float top_right_radius,
                            float bottom_right_radius,
                            float bottom_left_radius) {
-  if (width <= 0.0f || height <= 0.0f) {
-    return SkPath();
-  }
+  DCHECK(0.0f <= smoothness && smoothness <= 1.0f);
 
-  // We assume the radii are already constrained within the rectangle size
+  // Assume the radii are already constrained within the rectangle size
   DCHECK(top_left_radius + top_right_radius <= width);
   DCHECK(bottom_left_radius + bottom_right_radius <= width);
   DCHECK(top_left_radius + bottom_left_radius <= height);
   DCHECK(top_right_radius + bottom_right_radius <= height);
 
-  // Constrain each corner curve's smoothness
-  // TODO
+  if (width <= 0.0f || height <= 0.0f) {
+    return SkPath();
+  }
+
+  // Constrain the smoothness for each curve on each edge
+  auto [top_left_smoothness, top_right_smoothness] =
+      ConstrainSmoothness(width, smoothness, top_left_radius, top_right_radius);
+  auto [right_top_smoothness, right_bottom_smoothness] = ConstrainSmoothness(
+      height, smoothness, top_right_radius, bottom_right_radius);
+  auto [bottom_left_smoothness, bottom_right_smoothness] = ConstrainSmoothness(
+      width, smoothness, bottom_left_radius, bottom_right_radius);
+  auto [left_top_smoothness, left_bottom_smoothness] = ConstrainSmoothness(
+      height, smoothness, top_left_radius, bottom_left_radius);
 
   SkPath path;
 
   // Top left corner
-  DrawCorner(path, top_left_radius, CurveGeometry(top_left_radius, smoothness),
+  DrawCorner(path, top_left_radius,
+             CurveGeometry(top_left_radius, left_top_smoothness),
+             CurveGeometry(top_left_radius, top_left_smoothness),
              SkPoint::Make(x, y), 0);
 
   // Top right corner
   DrawCorner(path, top_right_radius,
-             CurveGeometry(top_right_radius, smoothness),
+             CurveGeometry(top_right_radius, top_right_smoothness),
+             CurveGeometry(top_right_radius, right_top_smoothness),
              SkPoint::Make(x + width, y), 1);
 
   // Bottom right corner
   DrawCorner(path, bottom_right_radius,
-             CurveGeometry(bottom_right_radius, smoothness),
+             CurveGeometry(bottom_right_radius, right_bottom_smoothness),
+             CurveGeometry(bottom_right_radius, bottom_right_smoothness),
              SkPoint::Make(x + width, y + height), 2);
 
   // Bottom left corner
   DrawCorner(path, bottom_left_radius,
-             CurveGeometry(bottom_left_radius, smoothness),
+             CurveGeometry(bottom_left_radius, bottom_left_smoothness),
+             CurveGeometry(bottom_left_radius, left_bottom_smoothness),
              SkPoint::Make(x, y + height), 3);
 
   path.close();
