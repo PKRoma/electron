5710396f7681c3baa3f47f19d7697dba70d32b26
John Kleinschmidt <kleinschmidtorama@gmail.com>
2026-02-19 17:09:24 -0500

    ci: test linux 64k

---
+++ script/qemu-init.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+echo "Mounting essential filesystems"
+mount -t proc proc /proc
+mount -t sysfs sys /sys
+mkdir -p /dev/pts
+mount -t devpts devpts /dev/pts
+mkdir -p /dev/shm
+mount -t tmpfs tmpfs /dev/shm
+mount -t tmpfs tmpfs /tmp
+chmod 1777 /tmp
+mount -t tmpfs tmpfs /run
+mkdir -p /run/dbus
+mkdir -p /run/user/0
+chmod 700 /run/user/0
+mount -t tmpfs tmpfs /var/tmp
+
+echo "Setting up machine-id for D-Bus"
+cat /proc/sys/kernel/random/uuid | tr -d '-' > /etc/machine-id
+
+echo "Setting system clock"
+date -s "$(cat /host-time)"
+
+echo "Setting up networking"
+ip link set lo up
+ip link set eth0 up
+ip addr add 10.0.2.15/24 dev eth0
+ip route add default via 10.0.2.2
+echo "nameserver 10.0.2.3" > /etc/resolv.conf
+
+echo "Loading 9p kernel modules"
+modprobe 9pnet
+modprobe 9pnet_virtio
+modprobe 9p
+
+echo "Mounting test files from host via 9p"
+mkdir -p /mnt/testfiles
+mount -t 9p -o trans=virtio,version=9p2000.L testfiles /mnt/testfiles
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export XDG_RUNTIME_DIR=/run/user/0
+
+echo "Starting entrypoint"
+echo "System: $(uname -s) $(uname -r) $(uname -m), page size: $(getconf PAGESIZE) bytes"
+ls -l /mnt/testfiles
+/mnt/testfiles/out/Default/electron --version
+EXIT_CODE=$?
+echo "Test execution finished with exit code $EXIT_CODE"
+echo $EXIT_CODE > /exit-code
+sync
+
+echo "Powering off"
+echo o > /proc/sysrq-trigger
